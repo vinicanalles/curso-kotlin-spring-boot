f13c6f5ef6162c67f650141a08b5b3ba6bf25b64
Vinicius Reia Canalles <vicanalles@gmail.com>
2022-02-24 14:35:58 -0300

    Configurando o banco de dados no projeto

--- mercado-livro/build.gradle.kts
+++ mercado-livro/build.gradle.kts
@@ -17,9 +17,13 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+	runtimeOnly("mysql:mysql-connector-java")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
